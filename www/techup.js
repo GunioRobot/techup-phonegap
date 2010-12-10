78ca67b789916ecd2d63a010685c2c1c068f5988
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2010-12-10 11:45:21 +0100

    added crawling

--- www/techup.js
+++ www/techup.js
@@ -1,4 +1,33 @@
- $(document).ready(function() {
-     alert('boing');
-   // do stuff when DOM is ready
- });
+(function ($) {
+    var getApiData, getApiDataSuccess, getApiDataFail;
+    
+    getApiData = function () {
+        var url, container;
+        // /api/event/$id.json
+        // /api/events/upcoming.json
+        // /api/events/past.json
+        // /api/user/$twittername.json
+
+        $.ajax({
+            url: 'http://techup.ch/api/events/upcoming.json',
+            success: getApiDataSuccess,
+            error: getApiDataFail,
+            dataType: 'json'
+        });
+
+    };
+    
+    getApiDataFail = function (e) {
+        alert('bang');
+        console.log(e);
+    };
+        
+    getApiDataSuccess = function (data) {
+        // $('body').html(data);
+		alert('got the stuff');
+		alert(data);
+		console.log(data);
+    };
+    getApiData();
+    
+}(jQuery));
