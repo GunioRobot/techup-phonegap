ae141729b24f404ce7d75baf06c6b09b68ff8af9
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2010-12-10 13:45:14 +0100

    add local storage

--- www/techup.js
+++ www/techup.js
@@ -1,12 +1,23 @@
 (function ($) {
-    var getApiData, getApiDataSuccess, getApiDataFail;
+    var getApiData, getApiDataSuccess, getApiDataFail, buildCurrentTimestamp, getlastRefresh, storeDataInStorage;
     
     getApiData = function () {
         var url, container;
+        
+        // Api definition
         // /api/event/$id.json
         // /api/events/upcoming.json
         // /api/events/past.json
         // /api/user/$twittername.json
+
+        // console.log(buildCurrentTimestamp());
+        // console.log(getlastRefresh() + 10);
+        if (buildCurrentTimestamp() < (getlastRefresh() + 5)) {
+            console.log('skipped');
+            return;
+        } else {
+            console.log('not skipped');
+        }
 
         $.ajax({
             url: 'http://techup.ch/api/events/upcoming.json',
@@ -23,9 +34,56 @@
     };
         
     getApiDataSuccess = function (data) {
-        // $('body').html(data);
-        console.log(data);
+        storeDataInStorage(data);
+    };
+    
+    buildCurrentTimestamp = function () {
+        var timestamp = new Date().getTime();
+        timestamp = parseInt(timestamp / 1000, 10);
+        return timestamp;
+    };
+    
+    getlastRefresh = function () {
+        var lastRefresh;
+        if (typeof(localStorage) == 'undefined') {
+            alert('Local storage not supported by this browser.');
+        }
+        lastRefresh = localStorage.getItem("lastRefresh");
+        if (lastRefresh === null) {
+            return 0;
+        }
+        return parseInt(lastRefresh, 10);
+    };
+    
+    storeDataInStorage = function (data) {
+        var timestamp = 0;
+        if (typeof(localStorage) == 'undefined') {
+            alert('Local storage not supported by this browser.');
+        }
+
+        // var mycount = localStorage.length;
+        
+        // $.forEach(data.events, function (id, value) {
+        //            
+        //        });
+
+        //this populates localStorage with a few links if there are none
+        // if (mycount<1) {
+        // timestamp = new Date().getTime();
+        //         timestamp = parseInt(timestamp / 1000, 10);
+        localStorage.setItem("events", data.events);
+        localStorage.setItem("lastRefresh", buildCurrentTimestamp());
+            // localStorage.setItem("Grand Rapids",
+            // "loadFlickr('72157621350530834','37054878@N03')");
+        // }
+
+        // for ( var i=0, len=mycount; i<len; i++ ){
+        //             var myitem = localStorage.key(i);
+        //             var mylink = localStorage.getItem(myitem); 
+        //             var myli = "<li><a href=\"javascript:" + mylink + "\">" + myitem + "</a></li>";
+        //             $('#nav').append(myli);
+        //         }
     };
     getApiData();
-    
+
 }(jQuery));
